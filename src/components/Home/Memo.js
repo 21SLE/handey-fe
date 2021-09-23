b9a5c54d5a07bbd00ba470edc18ef1c3eb87ec55
Hansle Lee <51855129+21SLE@users.noreply.github.com>
2021-09-23 17:48:51 +0900

    Revert "Feature sp4 home screen"

--- src/components/Home/Memo.js
+++ src/components/Home/Memo.js
@@ -1,53 +1,9 @@
-import axios from "axios";
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Memo.css";
 
-const baseUrl = "http://localhost:8080";
-
-function Memo({id, content}) {
-
-    const [memoTxt, setMemoTxt] = useState(content == null ? "" : content);
-
-    function changeMemoTxt(e) {
-        e.preventDefault();
-        setMemoTxt(e.target.value);
-    }
-
-    const getMemo = async () => {
-        // TODO 회원가입 완성시 수정
-        await axios.get(baseUrl + "/memo/1")
-            .then((response) => {
-                setMemoTxt(response.data.content === null ? "" : response.data.content);
-            })
-            .catch((error) => {
-                console.error("ERROR: " + error);
-            });
-    }
-
-    const onUpdateMemo = async (e) => {
-        if(e.key === 'Enter' && !e.shiftKey) {
-            e.preventDefault();
-            // TODO 회원가입 완성시 수정
-            await axios
-                    .put(baseUrl + "/memo/1", 
-                    {
-                        content: e.target.value
-                    })
-                    .then((response) => {
-                        console.log(response.data);
-                    })
-                    .catch((error) => {console.error(error);});
-            e.target.blur();
-            console.log("메모가 수정되었습니다.");
-        }
-    }
-
-    useEffect(() => {
-        getMemo();
-    }, []);
-
+function Memo() {
     return <div className="memo">
-        <textarea value = {memoTxt} onChange={changeMemoTxt} onKeyPress={onUpdateMemo} />
+        Memo Section
     </div>;
 }
 
