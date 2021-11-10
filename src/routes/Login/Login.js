0542c1ed6367dc71c85c8af09bc0c1bef90d7ecc
노수진 <shtnwls1111@ewhain.net>
2021-11-10 15:05:12 +0900

    login-not finished

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,49 +1,48 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import {useCookies} from "react-cookie";
 import "./Login.css";
-import { useHistory } from "react-router";
-
-function Login() {
-
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-
-    const handleID = (e) => {
-        e.preventDefault();
-        setEmail(e.target.value)
-    }
-
-    const handlePW = (e) =>{
-        e.preventDefault();
-        setPassword(e.target.value)
-    }
-    
-    const history = useHistory();
-    
-    useEffect(() => {
-        if (localStorage.getItem("user_info")){
-            history.push("/home")
-        }
-    })
 
     const baseUrl = "http://localhost:8080"
 
-        async function getLogin(){
+        function Login() {
 
-            console.log("click")
-            console.log("Id: ", email)
-            console.log("PW: ", password) 
+            const [email, setEmail] = useState("");
+            const [password, setPassword] = useState("");
+        
+            const handleID = (e) => {
+                e.preventDefault();
+                setEmail(e.target.value)
+            }
+        
+            const handlePW = (e) =>{
+                e.preventDefault();
+                setPassword(e.target.value)
+            }
 
-            await axios
-             .post(baseUrl+"/login")
-             .then(res => {
-                localStorage.setItem('token',res.data.token);
-            })
-            .catch((error)=>{
-                console.error(error)
-            })
-        }
+            const [cookies, setCookie, removeCookie ] = useCookies(['Token']); //쿠키 이름을 Token으로 관리
+            // 쿠키 값 출력 , 설정 , 삭제
 
+            const onsubmit = (data) => {
+                axios
+                .post(baseUrl+"/login",data)
+                .then(function(response){
+                    alert(response.data.message);
+                    if(response.data.loginSuccess) {
+                        // setuserLogin(response.data.loginSuccess)
+                        axios.defaults.headers.common['token'] = response.data.token; //토큰을 axios에 저장시켜 axios로 전송시 token도 같이 보냄
+                        setCookie('Token', response.data.token, { path: '/home' }); 
+                        // 페이지에서 쿠키에 엑세스할 수 있게 함
+                    }
+                    })
+                    .catch(function (error) {
+                        // handle error
+                        console.log(error);
+                    });
+            }
+        
+
+        
     return(
         <div className = "InputBox">
             <h1 className = "title">Sign in</h1>
@@ -67,7 +66,7 @@ function Login() {
             placeholder = "password"
             />
             </div>
-            <button type = "button" onClick={getLogin}>Login</button>
+            <button type = "button" onClick={onsubmit}>Login</button>
             <div className = "findpw">
                 <a href = "/findPw">Forget Password?</a>
                 <a href = "/join"> / Join</a>
