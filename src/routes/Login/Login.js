17e0fb6c7183854c96f3236e6b7e609bcee47ffb
노수진 <shtnwls1111@ewhain.net>
2021-11-14 13:45:01 +0900

    login token o

--- src/routes/Login/Login.js
+++ src/routes/Login/Login.js
@@ -1,12 +1,11 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
-import {useCookies} from "react-cookie";
 import "./Login.css";
 
     const baseUrl = "http://localhost:8080"
 
         function Login() {
-
+            
             const [email, setEmail] = useState("");
             const [password, setPassword] = useState("");
         
@@ -20,29 +19,28 @@ import "./Login.css";
                 setPassword(e.target.value)
             }
 
-            const [cookies, setCookie, removeCookie ] = useCookies(['Token']); //쿠키 이름을 Token으로 관리
-            // 쿠키 값 출력 , 설정 , 삭제
+            
+            const onsubmit = async() => {
+                
+                const User= {
+                    email: email,
+                    password: password
+                };
 
-            const onsubmit = (data) => {
-                axios
-                .post(baseUrl+"/login",data)
-                .then(function(response){
-                    alert(response.data.message);
-                    if(response.data.loginSuccess) {
-                        // setuserLogin(response.data.loginSuccess)
-                        axios.defaults.headers.common['token'] = response.data.token; //토큰을 axios에 저장시켜 axios로 전송시 token도 같이 보냄
-                        setCookie('Token', response.data.token, { path: '/home' }); 
-                        // 페이지에서 쿠키에 엑세스할 수 있게 함
-                    }
-                    })
-                    .catch(function (error) {
-                        // handle error
-                        console.log(error);
-                    });
-            }
-        
+            const res = 
+                await axios
+                .post(baseUrl+"/login",User)    
+                localStorage.jwtAuthToken = res.headers['ACCESS_TOKEN'];
+                console.log(res.data);
 
-        
+                if(localStorage.jwtAuthToken.ACCESS_TOKEN == null){
+                    alert("fail");
+                    console.log("fail");
+                }
+                else{
+                    console.log("success")
+                }
+            }       
     return(
         <div className = "InputBox">
             <h1 className = "title">Sign in</h1>
