94c813fbe1268d6aecb982744ae2766b53beeaf3
tbdpa <tbdpapdl@gmail.com>
2021-11-16 02:33:36 +0900

    .

---
+++ src/components/History/AfterHst.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./AfterHst.css";
+
+class AfterHst extends React.Component {
+    render() {
+        return <div className = "AfterHst-layout">
+            <span>AfterHst</span>
+        </div>;
+    }
+}
+export default AfterHst;
---
+++ src/components/History/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Search.css";
+
+class Search extends React.Component {
+    render() {
+        return <div className = "Search-layout">
+            <span>Search</span>
+        </div>;
+    }
+}
+export default Search;
