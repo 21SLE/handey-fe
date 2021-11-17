9d42fb8511560fba8dd1b6c8661796f390e58bb2
tbdpa <tbdpapdl@gmail.com>
2021-11-18 03:48:43 +0900

    .

--- src/components/History/HstBoxList.js
+++ src/components/History/HstBoxList.js
@@ -3,38 +3,48 @@ import axios from "axios";
 import HstBox from "../../components/History/HstBox";
 import "./HstBoxList.css";
 
-const baseUrl = "http://localhost:8080";
+//const baseUrl = "http://localhost:8080";
 
-export const HstBoxContext = React.createContext();
+//export const HstBoxContext = React.createContext();
 
-function HstBoxList() {
+// function HstBoxList() {
 
-    const [HstBoxListData, setHstBoxListData] = useState([]); 
+//     const [HstBoxListData, setHstBoxListData] = useState([]); 
 
-    useEffect(() => {
-        getHstBoxList();
-    }, []);
+//     useEffect(() => {
+//         getHstBoxList();
+//     }, []);
 
-    async function getHstBoxList() {
+//     async function getHstBoxList() {
 
-        await axios.get(baseUrl + "/history/todos")
-            .then((response) => {
-                console.log(response.data);
-                setHstBoxListData(response.data);
-            })
-            .catch((error) => {
-                console.error("ERROR: " + error);
-            });
+//         await axios.get(baseUrl + "/history/todos")
+//             .then((response) => {
+//                 console.log(response.data);
+//                 setHstBoxListData(response.data);
+//             })
+//             .catch((error) => {
+//                 console.error("ERROR: " + error);
+//             });
         
-        await axios.get(baseUrl + "/afterList")
-            .then((response) => {
-                console.log(response.data);
-                setHstBoxListData(response.data);
-            })
-            .catch((error) => {
-                console.error("ERROR: " + error);
-            });
-    };
+//         await axios.get(baseUrl + "/afterList")
+//             .then((response) => {
+//                 console.log(response.data);
+//                 setHstBoxListData(response.data);
+//             })
+//             .catch((error) => {
+//                 console.error("ERROR: " + error);
+//             });
+
+
+//     };
+
+// }
+class HstBoxList extends React.Component {
+    render() {
+        return <div className = "HstBoxList-layout">
+            <span>HstBoxList</span>
+        </div>;
+    }
 }
 
 export default HstBoxList;
--- src/components/History/Search.js
+++ src/components/History/Search.js
@@ -13,69 +13,50 @@ const baseUrl = "http://localhost:8080";
     // const [hstElms, setHstElms] = useState(hstBoxList);
     // const [searchTerm, setSearchTerm] = useState('')
 
-class Search extends React.Component {
+//class Search extends React.Component {
     
-    constructor() {
-        super();
+    // constructor() {
+    //     super();
 
-        this.state={
-        search:null
-        };
-    }
+    //     this.state={
+    //     search:null
+    //     };
+    // }
 
-    searchSpace=(e)=>{
-        let keyword = e.target.value;
-        this.setState({search:keyword})
-    }
+    // searchSpace=(e)=>{
+    //     let keyword = e.target.value;
+    //     this.setState({search:keyword})
+    // }
 
+
+    //render() {        
+        // const items = HstBoxList.map(data=>{
+        //   return(
+        //   <div>
+        //     <ul>
+        //       <li>
+        //         <span>{data}</span>
+        //       </li>
+        //     </ul>
+        //   </div>
+        //   )
+        // })
+        //onChange={(e)=>this.setSearchTerm(e)} 
+
+//         return <div className = "Search"> 
+//             <div className="Search-layout">
+//                     <input type="text" className = "Search_input" placeholder="검색" />
+//             </div>
+//         </div> 
+//     }
+// }
+
+class Search extends React.Component {
     render() {
-        const items = HstBoxList.map(data=>{
-          return(
-          <div>
-            <ul>
-              <li>
-                <span>{data}</span>
-              </li>
-            </ul>
-          </div>
-          )
-        })
-
-    // const items = HstBoxList.map(data=>{
-    //     return(
-    //     <div>
-    //       <ul>
-    //         <li style={{position:'relative',left:'10vh'}}>
-    //           <span>{data.name}</span>
-    //           <span>{data.age}</span>
-    //           <span>{data.country}</span>
-    //         </li>
-    //       </ul>
-    //     </div>
-    //     )
-    //   })
-                // .filter((val)=> {
-                //     if (searchTerm == "") {
-                //         return val
-                //     } else if (val.sta_nm.toLowerCase().includes(searchTerm.toLowerCase()) || val.sido_nm.toLowerCase().includes(searchTerm.toLowerCase())) {
-                //         return val}
-                // })
-
-        return (
-            <div className = "Search">
-                <input type="text" className = "Search_input" placeholder="검색" onChange={(e)=>this.setSearchTerm(e)}/>
-                    <div className="Search-layout">
-                    {/* {HstBoxList.map((val, key) => {
-                        return (
-                            <div className="history" key={key}>
-                                <p> {val.sta_nm} </p>
-                            </div>
-                        );
-                    })} */}
-                    <span> {items} </span>
-                    </div>
-            </div>
-        )
+        return <div className = "Search-layout">
+            < input type="text" className = "Search_input"/> 
+        </div>;
     }
 }
+
 export default Search;
