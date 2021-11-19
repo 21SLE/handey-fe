134b8e4cfe9119957dc5af37ca5f1e984e065db4
tbdpa <tbdpapdl@gmail.com>
2021-11-20 04:50:42 +0900

    .

--- src/routes/Home/Home.js
+++ src/routes/Home/Home.js
@@ -5,6 +5,34 @@ import Memo from "../../components/Home/Memo";
 import WeeklyBoxList from "../../components/Home/WeeklyBoxList";
 import "./Home.css";
 
+const account = publicClientApplication.getAllAccounts()[0];
+
+const accessTokenRequest = {
+    scopes: ["user.read"],
+    account: account
+}
+
+publicClientApplication.acquireTokenSilent(accessTokenRequest).then(function(accessTokenResponse) {
+    // Acquire token silent success
+    let accessToken = accessTokenResponse.accessToken;
+    // Call your API with token
+    callApi(accessToken);
+}).catch(function (error) {
+    //Acquire token silent failure, and send an interactive request
+    if (error instanceof InteractionRequiredAuthError) {
+        publicClientApplication.acquireTokenPopup(accessTokenRequest).then(function(accessTokenResponse) {
+            // Acquire token interactive success
+            let accessToken = accessTokenResponse.accessToken;
+            // Call your API with token
+            callApi(accessToken);
+        }).catch(function(error) {
+            // Acquire token interactive failure
+            console.log(error);
+        });
+    }
+    console.log(error);
+});
+
 class Home extends React.Component {
     
 
