4a63fa33a3fad8c6b149bcc10bc9160396352c08
Hansle Lee <hssarah13@gmail.com>
2021-12-17 16:59:46 +0900

    welcomepage 이미지 추가, css 수정

--- src/routes/Join/WelcomeJoin.js
+++ src/routes/Join/WelcomeJoin.js
@@ -1,5 +1,7 @@
 import React from "react"
 import "./WelcomeJoin.css"
+import CelebrateIcon1 from "../../components/common/img/celebrate_icon1.png"
+import CelebrateIcon2 from "../../components/common/img/celebrate_icon2.png"
 
 function WelcomeJoin() {
 
@@ -17,10 +19,16 @@ function WelcomeJoin() {
     <div className="welcomejoin-layout__circle">
         <div className = "welcomejoin-layout__wrap">
             <h1 className="logo">HANDEY</h1>
-            회원가입을 축하드립니다!
-            <button type="button" onClick = {moveToLoginPage}>로그인하러가기</button>
+            
+            <button className="loginButton" type="button" onClick = {moveToLoginPage}>로그인하러가기</button>
+        </div>
+        <div className="celebration__section">
+            <img className="celebrate_icon" alt=" " src={CelebrateIcon1} />
+            <h2>회원가입을 축하드립니다!</h2>
+            <img className="celebrate_icon" alt=" " src={CelebrateIcon2} />
         </div>
     </div>
+    
 </div>
 
 }
