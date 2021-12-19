4ffd1b153442722241cd8687ecbc5d158772569a
Hansle Lee <hssarah13@gmail.com>
2021-12-19 19:13:03 +0900

    calendar css

--- src/components/common/SideBarCalendar.js
+++ src/components/common/SideBarCalendar.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Calendar from "react-calendar";
-// import 'react-calendar/dist/Calendar.css';
-import "./SideBarCalendar.css"
+import 'react-calendar/dist/Calendar.css';
+import "./SideBarCalendar.css";
+import dayjs from "dayjs";
 
 const SideBarCalendar = () => {
-    return <Calendar>
+    return <Calendar 
+    formatDay ={(locale, date) => dayjs(date).format('D')}>
 
     </Calendar>
 }
