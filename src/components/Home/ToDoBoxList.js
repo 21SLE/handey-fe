22228d81aa58c521c36f7e7a705eb29e86724400
Hansle Lee <hssarah13@gmail.com>
2022-01-22 17:46:22 +0900

    19인치 미디어 쿼리

--- src/components/Home/ToDoBoxList.js
+++ src/components/Home/ToDoBoxList.js
@@ -89,7 +89,8 @@ function ToDoBoxList({accessToken, userId}) {
     const [, drop] = useDrop(() => ({ accept: ItemTypes.ToDoBox }));
 
     const breakpoints = {
-        default: 2,
+        default: 3,
+        1600: 2,
         1340: 1
     }
 
